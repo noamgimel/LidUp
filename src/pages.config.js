b0778c7957d912fabd5ccfcc83597d68e4af7c2e
נועם גמליאל <noam.gamliel@gmail.com>
2026-01-21 15:34:03 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -4,14 +4,14 @@ import Dashboard from './pages/Dashboard';
 import GoogleAuthSuccess from './pages/GoogleAuthSuccess';
 import Home from './pages/Home';
 import Integrations from './pages/Integrations';
+import MasterAdminDashboard from './pages/MasterAdminDashboard';
 import Meetings from './pages/Meetings';
+import NoAccess from './pages/NoAccess';
 import Reports from './pages/Reports';
+import SelectWorkspace from './pages/SelectWorkspace';
 import Settings from './pages/Settings';
 import WorkStageSettings from './pages/WorkStageSettings';
-import MasterAdminDashboard from './pages/MasterAdminDashboard';
 import WorkspaceManagement from './pages/WorkspaceManagement';
-import NoAccess from './pages/NoAccess';
-import SelectWorkspace from './pages/SelectWorkspace';
 import __Layout from './Layout.jsx';
 
 
@@ -22,14 +22,14 @@ export const PAGES = {
     "GoogleAuthSuccess": GoogleAuthSuccess,
     "Home": Home,
     "Integrations": Integrations,
+    "MasterAdminDashboard": MasterAdminDashboard,
     "Meetings": Meetings,
+    "NoAccess": NoAccess,
     "Reports": Reports,
+    "SelectWorkspace": SelectWorkspace,
     "Settings": Settings,
     "WorkStageSettings": WorkStageSettings,
-    "MasterAdminDashboard": MasterAdminDashboard,
     "WorkspaceManagement": WorkspaceManagement,
-    "NoAccess": NoAccess,
-    "SelectWorkspace": SelectWorkspace,
 }
 
 export const pagesConfig = {
