281345c2740a2651d7028554d6e6fca7a3bce92f
נועם גמליאל <noam.gamliel@gmail.com>
2026-01-25 14:27:02 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -4,14 +4,10 @@ import Dashboard from './pages/Dashboard';
 import GoogleAuthSuccess from './pages/GoogleAuthSuccess';
 import Home from './pages/Home';
 import Integrations from './pages/Integrations';
-import MasterAdminDashboard from './pages/MasterAdminDashboard';
 import Meetings from './pages/Meetings';
-import NoAccess from './pages/NoAccess';
 import Reports from './pages/Reports';
-import SelectWorkspace from './pages/SelectWorkspace';
 import Settings from './pages/Settings';
 import WorkStageSettings from './pages/WorkStageSettings';
-import WorkspaceManagement from './pages/WorkspaceManagement';
 import __Layout from './Layout.jsx';
 
 
@@ -22,14 +18,10 @@ export const PAGES = {
     "GoogleAuthSuccess": GoogleAuthSuccess,
     "Home": Home,
     "Integrations": Integrations,
-    "MasterAdminDashboard": MasterAdminDashboard,
     "Meetings": Meetings,
-    "NoAccess": NoAccess,
     "Reports": Reports,
-    "SelectWorkspace": SelectWorkspace,
     "Settings": Settings,
     "WorkStageSettings": WorkStageSettings,
-    "WorkspaceManagement": WorkspaceManagement,
 }
 
 export const pagesConfig = {
